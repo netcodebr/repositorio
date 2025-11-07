f222fa3a71f8421d36c8a9cd5881f384ab49b5c0
Nelson Ferreira <nelson.pessoal.2022jr@gmail.com>
2025-11-07 20:46:48 -0300

    Update service-worker.js

--- RÁDIOFM/service-worker.js
+++ RÁDIOFM/service-worker.js
@@ -1,1 +1,28 @@
+const CACHE_NAME = "metropolitana-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
 
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES_TO_CACHE)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
